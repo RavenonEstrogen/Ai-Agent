7d345bde7069da83f01a5564e0ee9c130bf250ef
RavenonEstrogen <lizbethpep30@outlook.com>
2025-07-28 10:58:32 -0400

    adds a function that allows to overwrite and create new files

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -40,3 +40,20 @@ def get_file_content(working_directory, file_path):
                 return file
     except Exception as e:
         return f"Error: {str(e)}"
+    
+def write_file(working_directory, file_path, content):
+    joined_path = os.path.join(working_directory, file_path)
+
+    if not os.path.abspath(joined_path).startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    if os.path.dirname(joined_path):
+        if not os.path.exists(os.path.dirname(joined_path)):
+            os.makedirs(os.path.dirname(joined_path))
+
+    try:
+        with open(joined_path, "w") as f:
+            f.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,13 +1,10 @@
-from functions.get_files_info import get_files_info, get_file_content
+from functions.get_files_info import get_files_info, get_file_content, write_file
 
-test_1 = get_file_content("calculator", "main.py")
+test_1 = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
 print(test_1)
 
-test_2 = get_file_content("calculator", "pkg/calculator.py")
+test_2 = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
 print(test_2)
 
-test_3 = get_file_content("calculator", "/bin/cat")
+test_3 = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
 print(test_3)
-
-test_4 = get_file_content("calculator", "pkg/does_not_exist.py")
-print(test_4)
